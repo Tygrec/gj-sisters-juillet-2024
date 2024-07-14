fa6def1e6950ee4aef8386bb512b7acc006dc613
Tygrec <coline.tournus@gmail.com>
2024-07-14 09:56:37 +0200

    Revert "Revert "Caméras corrigées, movebox corrigée""
    This reverts commit 44a96bc985aad80ab30590eabfb2808c6a201d55.

---
+++ Assets/MoveBox.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBox : MonoBehaviour
+{
+    public void MoveToLocation() {
+        UiManager.instance.MoveToLocation();
+    }
+}
